ae60173767bcb0c57d962066c62146bbf4c4e763
dzieszk <tobi.dzieszk@gmail.com>
2018-05-10 08:08:07 +0200

    niedziała

--- app/components/videoView/videoView.component.js
+++ app/components/videoView/videoView.component.js
@@ -5,6 +5,7 @@ angular.module('app').component('videoView', {
     templateUrl: 'components/videoView/videoView.template.html',
     controller: function videoView($scope, ytService, historyService) {
         this.link="https://youtube.com/embed/" + this.id;
+        this.suggestions = [];
 
         this.$onInit = () => {
             ytService.watch(this.id).then(data => {
@@ -16,19 +17,18 @@ angular.module('app').component('videoView', {
                 this.likes = this.item.statistics.likeCount;
                 this.dislikes = this.item.statistics.dislikeCount;
                 ytService.tags(data.data.items[0].snippet.tags[0]).then(data => {
-                    console.log(data);
+                    this.suggestions = data.data.items;
+                    console.log(this.suggestions);
                 });
                 historyService.add(this.item);
-                console.log(this.item);
             });
         };
-        this.playlist = [];
-        this.suggestions = [];
-        const song = {title: 'The best princess is Luna', channel: 'Starlight Glimmer', thumb: '../../img/1042.jpg'};
-        this.playlist.push(song);
-        for (var i=0; i<6; i++) {
-            song.id = i;
-            this.suggestions.push(song);
-        }
+        // this.playlist = [];
+        // const song = {title: 'The best princess is Luna', channel: 'Starlight Glimmer', thumb: '../../img/1042.jpg'};
+        // this.playlist.push(song);
+        // for (var i=0; i<6; i++) {
+        //     song.id = i;
+        //     this.suggestions.push(song);
+        // }
     }
 });
