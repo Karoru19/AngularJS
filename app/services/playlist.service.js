f40b00f44350e85772e593abce15b64350dde1b4
dzieszk <tobi.dzieszk@gmail.com>
2018-05-10 08:32:49 +0200

    service playlist

--- app/services/playlist.service.js
+++ app/services/playlist.service.js
@@ -1,7 +1,13 @@
 angular.module('app').service('playlistService', function($http, config){
     this.playlist = [];
     console.log("init playlist service");
-    this.add = id => {
-        this.playlist.add(id);
+    this.add = item => {
+        this.playlist.push(item);
+    };
+    this.remove = index => {
+        this.playlist.splice(index, 1);
+    };
+    this.get = () => {
+        return this.playlist;
     };
 });
